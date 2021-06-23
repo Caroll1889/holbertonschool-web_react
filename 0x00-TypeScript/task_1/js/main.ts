5024087eb9e3aab3adb7cfe41805db691e46b8d3
Caroll1889 <diahancaroll@hotmail.com>
2021-06-22 19:00:49 -0500

    Let's build a Teacher interface

--- 0x00-TypeScript/task_1/js/main.ts
+++ 0x00-TypeScript/task_1/js/main.ts
@@ -1,8 +1,10 @@
 interface Teacher {
-    firstName: string;
-    lastName: string;
-    fullTimeEmployee: boolean;
-    location: string;
-    yearsOfExperience?: number;
-    [propName: string]: any;
+  firstName: string;
+  lastName: string; 
+  fullTimeEmployee: boolean;
+  location: string;
+  yearsOfExperience?: number;
+  /* eslint-disable @typescript-eslint/no-explicit-any */
+  [propName: string]: any;
+  /* eslint-enable @typescript-eslint/no-explicit-any */
 }
