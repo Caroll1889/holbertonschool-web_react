744a7daad00a8795d114fda09764d9624088c4e6
Caroll1889 <diahancaroll@hotmail.com>
2021-07-15 21:59:30 -0500

    Fix: test notification component

--- 0x05-React_inline_styling/task_1/dashboard/src/Notifications/Notifications.js
+++ 0x05-React_inline_styling/task_1/dashboard/src/Notifications/Notifications.js
@@ -1,9 +1,27 @@
 import React from "react";
 import PropTypes from "prop-types";
+import { css, StyleSheet } from 'aphrodite';
 import NotificationItem from "./NotificationItem";
 import closeButton from "../assets/close-icon.png";
 import NotificationItemShape from "./NotificationItemShape";
 import "./Notifications.css";
+
+const styles = StyleSheet.create({
+  notifications: {
+    border: '3px dashed #e14852',
+    padding: '1em',
+    float: 'right',
+    margin: '10px 30px 0 0'
+  },
+  menuItem: {
+    textAlign: 'right',
+    marginRight: '30px'
+  },
+  notificationsButtonImage: {
+    width: '20px'
+  }
+})
+
 
 class Notifications extends React.Component {
   constructor (props) {
@@ -24,28 +42,31 @@ class Notifications extends React.Component {
   render () { 
     return (
       <>
-        <div className="menuItem">Your notifications</div>
-
+        <div className='menuItem'><div className={css(styles.menuItem)}>Your notifications</div></div>
+    
         { this.props.displayDrawer ? 
-          (<div className="Notifications">
-            <button
-              style={{
-                right: 45,
-                border: "none",
-                position: "absolute",
-                background: "transparent",
-              }}
-              aria-label="close"
-              onClick={() => console.log("Close button has been clicked")}
-            >
-              <img src={closeButton} alt="close button icon" />
-            </button>
-            <p>Here is the list of notifications</p>
-            <ul>
-              {this.props.listNotifications.length === 0 ? (<NotificationItem value='No new notification for now' type='no-new' />) : <></>}
-              {this.props.listNotifications.map((not) => (<NotificationItem key={not.id} type={not.type} value={not.value} html={not.html} markAsRead={() => {this.markAsRead(not.id)}} />))}
-            </ul>
-          </div>) 
+          (<div className='Notifications'>
+            <div className={css(styles.notifications)}>
+              <button
+                style={{
+                  right: 45,
+                  border: "none",
+                  position: "absolute",
+                  background: "transparent",
+                }}
+                aria-label="close"
+                onClick={() => console.log("Close button has been clicked")}
+              >
+              <img src={closeButton} className={css(styles.notificationsButtonImage)} alt="close button icon" />
+              </button>
+              <p>Here is the list of notifications</p>
+              <ul>
+                {this.props.listNotifications.length === 0 ? (<NotificationItem value='No new notification for now' type='no-new' />) : <></>}
+                {this.props.listNotifications.map((not) => (<NotificationItem key={not.id} type={not.type} value={not.value} html={not.html} markAsRead={() => {this.markAsRead(not.id)}} />))}
+              </ul>
+            </div>
+          </div>)
+             
           : <></>
         }
       </>
