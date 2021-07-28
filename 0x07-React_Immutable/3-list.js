66e6ada33457136eda8a4d50db283939b4f1904d
Caroll1889 <diahancaroll@hotmail.com>
2021-07-28 14:09:14 -0500

    List and push

--- 0x07-React_Immutable/3-list.js
+++ 0x07-React_Immutable/3-list.js
@@ -1,9 +1,10 @@
-import { List } from 'immutable';
+import immutable from 'immutable';
+const { List } = immutable;
 
 export function getListObject(array) {
-    return List(array)
+    return List(array);
 }
 
 export function addElementToList(list, element) {
-    return list.push(element)
+    return list.push(element);
 }
