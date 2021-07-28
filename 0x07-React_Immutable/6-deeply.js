37ab3925ae06ccf481de1a6a0e257a6923e4629c
Caroll1889 <diahancaroll@hotmail.com>
2021-07-28 17:01:39 -0500

    Nested merge with mergeDeep

--- 0x07-React_Immutable/6-deeply.js
+++ 0x07-React_Immutable/6-deeply.js
@@ -2,9 +2,10 @@ import immutable from 'immutable';
 
 const { Map } = immutable;
 
-export function mergeDeeplyElements(page1, page2) {
-    const obj1 = Map(page1);
-    const obj2 = Map(page2);
-    const obj3 = obj1.mergeDeep(obj2)
-    return obj3
+export default function mergeDeeplyElements(page1, page2) {
+    return Map(page1).mergeDeep(Map(page2))
+    // const obj1 = Map(page1);
+    // const obj2 = Map(page2);
+    // const obj3 = obj1.mergeDeep(obj2);
+    // return obj3; 
 }
