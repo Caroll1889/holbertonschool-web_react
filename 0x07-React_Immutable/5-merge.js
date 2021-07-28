9172b86245994085fac8a29b1364ccd42a97dcb6
Caroll1889 <diahancaroll@hotmail.com>
2021-07-28 16:40:48 -0500

    Merge & concat

--- 0x07-React_Immutable/5-merge.js
+++ 0x07-React_Immutable/5-merge.js
@@ -3,14 +3,14 @@ import immutable from 'immutable';
 const { Map, List } = immutable;
 
 
-export default function concatElements(page1, page2) {
+export function concatElements(page1, page2) {
     const list1 = List(page1);
     const list2 = List(page2);
     const list3 = list1.concat(list2);
     return list3;
 }
 
-export default function mergeElements(page1, page2) {
+export function mergeElements(page1, page2) {
     const obj1 = Map(page1);
     const obj2 = Map(page2);
     const obj3 = obj1.merge(obj2);  
