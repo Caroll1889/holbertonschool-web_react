df886fc643018c42e5be88291c7deab36a4e568d
Caroll1889 <diahancaroll@hotmail.com>
2021-07-28 17:13:16 -0500

    Change: from declarative function to arrow function

--- 0x07-React_Immutable/2-nested.js
+++ 0x07-React_Immutable/2-nested.js
@@ -1,5 +1,9 @@
 import {fromJS, getIn } from 'immutable';
 
-export default function accessImmutableObject(object, array) {
-  return getIn(fromJS(object), array);
-}
+// export default function accessImmutableObject(object, array) {
+//   return getIn(fromJS(object), array);
+// }
+
+const accessImmutableObject = (object, array) => getIn(fromJS(object), array);
+
+export default accessImmutableObject;
