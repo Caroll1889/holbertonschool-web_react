54224d80e49af273906e46af3820f48bd4d8a3e4
Caroll1889 <diahancaroll@hotmail.com>
2021-07-29 19:55:34 -0500

    Fix: eslint

--- 0x07-React_Immutable/2-nested.js
+++ 0x07-React_Immutable/2-nested.js
@@ -1,8 +1,5 @@
 import {fromJS, getIn } from 'immutable';
 
-// export default function accessImmutableObject(object, array) {
-//   return getIn(fromJS(object), array);
-// }
 
 const accessImmutableObject = (object, array) => getIn(fromJS(object), array);
 
