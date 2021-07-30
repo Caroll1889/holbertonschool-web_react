131c1cedf8876fdb1e01275eee15ecac3127d279
Caroll1889 <diahancaroll@hotmail.com>
2021-07-29 20:22:37 -0500

    Fix: eslint

--- 0x07-React_Immutable/6-deeply.js
+++ 0x07-React_Immutable/6-deeply.js
@@ -3,9 +3,5 @@ import immutable from 'immutable';
 const { Map } = immutable;
 
 export default function mergeDeeplyElements(page1, page2) {
-    return Map(page1).mergeDeep(Map(page2))
-    // const obj1 = Map(page1);
-    // const obj2 = Map(page2);
-    // const obj3 = obj1.mergeDeep(obj2);
-    // return obj3; 
+  return Map(page1).mergeDeep(Map(page2));
 }
